1e90be6e2e629524f328fd9b686ea312ab9d73a6
tina <tinaw425@gmail.com>
2017-08-02 20:27:55 -0400

    initial sign in and login forms

--- app/views.py
+++ app/views.py
@@ -1,7 +1,32 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html',title='Home')
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+    	flash('Login successful!')
+    	return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
