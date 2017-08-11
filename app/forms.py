1999035a30c50021e2d79501a2128e062d26ae3e
srafi1 <shakilrafi0@gmail.com>
2017-08-11 19:19:32 -0400

    made signup card based

--- app/forms.py
+++ app/forms.py
@@ -6,32 +6,34 @@ from .models import User
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField()
 
 class SignupForm(Form):
-	firstname = StringField('firstname', validators=[DataRequired()])
-	lastname = StringField('lastname', validators=[DataRequired()])
-	nickname = StringField('nickname', validators=[DataRequired()])
-	username = StringField('username', validators=[DataRequired()])
-	password = PasswordField('password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
-	password2 = PasswordField('password2', validators=[DataRequired()])
-	email = StringField('email', validators=[DataRequired(), Email()])
-	submit = SubmitField()
+    firstname = StringField('firstname', validators=[DataRequired()])
+    lastname = StringField('lastname', validators=[DataRequired()])
+    nickname = StringField('nickname', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('password2', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    submit = SubmitField()
 
-	def validate_on_submit(self):
-		if not Form.validate_on_submit(self):
-			return False
+    def validate_on_submit(self):
+        if not Form.validate_on_submit(self):
+            return False
 
-		isError = False
-		if User.query.filter_by(email=self.email.data).first():
-			self.email.errors.append("This email is already in use.")
-			isError = True
+        isError = False
 
-		if User.query.filter_by(username = self.username.data).first():
-			self.username.errors.append("This username is already in use.")
-			isError = True
+        if User.query.filter_by(email=self.email.data).first():
+            self.email.errors.append("This email is already in use.")
+            isError = True
 
-		return not isError
+        if User.query.filter_by(username = self.username.data).first():
+            self.username.errors.append("This username is already in use.")
+            isError = True
+
+        return not isError
 
 class EditForm(Form):
-	nickname = StringField('nickname', validators=[DataRequired()])
-	about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
