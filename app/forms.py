8bc5f97faca46a840f6bd2016b31b0a2df987259
tina <tinaw425@gmail.com>
2017-08-12 13:33:16 -0400

    added user posts

--- app/forms.py
+++ app/forms.py
@@ -37,3 +37,7 @@ class SignupForm(Form):
 class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+
+class PostForm(Form):
+    post = StringField('post', validators=[DataRequired()])
+    
